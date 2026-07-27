33e0a714a09c5251aebf9ac89ce3fdb43f5a8cb1
Muhammad_Abuzar <abuzarabid.py@gmail.com>
2026-07-27 20:33:25 +0500

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,39 @@
+# # # #     #  function no: 01 
+# # # # # def last_char(name):
+# # # # #     return name[-1]
+
+# # # # # print(last_char("Abuzar"))
+
+# # # #     #  function no: 02
+# # # # def even_odd(num):
+# # # #     if num%2== 0:
+# # # #         return "Even"
+# # # #     else:
+# # # #         return "Odd"
+
+# # # # print(even_odd(6))
+
+# # #       #   function no: 03
+# # # def even_odd(num):
+# # #     if num%2== 0:
+# # #         return "Even"
+# # #     return "Odd"
+
+# # # print(even_odd(8))
+
+
+# #         # function no: 04
+# # def is_even(num):
+# #     if num%2== 0:
+# #         return True
+# #     return False
+
+# # print(is_even(9))
+
+#           #   function no: 05
+
+# def odd_even(num):
+#     return num%2== 0
+
+# print(odd_even(11))
+
