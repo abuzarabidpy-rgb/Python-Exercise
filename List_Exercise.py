896d2cf6e52eb34461d1236bdb7264fb777ee173
Muhammad_Abuzar <abuzarabid.py@gmail.com>
2026-08-08 22:31:41 +0500

    list_Exercise.py
    how we can use list_function in python

---
+++ List_Exercise.py
@@ -0,0 +1,26 @@
+# list
+# List no : 01
+fruits = ["apple", "banana", "cherry"]
+print(fruits[:1])
+
+# # list no : 02
+friends = ["irfan", "Anas"]
+print(friends)
+
+# list no : 03
+numbers = [1, 2, 3, 4 ]
+print(numbers)
+
+# list no : 04
+mixed_list = [ 1, 2, 3, 4, "apple", "cherry", 1.40, ]
+mixed_list[3] = "five"
+print(mixed_list)
+
+# list no : 05
+vegetables = ["potato", "tomato", "onion"]
+print(vegetables)
+
+# list no : 06
+colors = ["red", "yellow","blue"]
+print(colors)
+
