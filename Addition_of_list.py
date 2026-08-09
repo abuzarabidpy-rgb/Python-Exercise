f6b27515211ed0fbb75b52899761202cbb80c6ff
Muhammad_Abuzar <abuzarabid.py@gmail.com>
2026-08-09 21:57:54 +0500

    Addition of list
    this function tells that how we can add the list

---
+++ Addition_of_list.py
@@ -0,0 +1,37 @@
+# add values in list
+
+# list no : 01
+fruits = ["grapes", "apple"]
+fruits.append("mango")
+print(fruits)
+
+# list no : 02
+fruits = []
+fruits.append("mango")
+fruits.append("grapes")
+fruits.append("cherry")
+print(fruits)
+
+# list no : 03
+names = ["Abuzar", "Irfan"]
+names.append("Anas")
+print(names)
+
+# list no : 04
+names = []
+names.append("Abuzar")
+names.append("Irfan")
+names.append("Anas")
+print(names)
+
+# list no : 05
+vegetables = ["potato", "tomato"]
+vegetables.append("carrot")
+print(vegetables)
+
+# list no : 05
+vegetables = []
+vegetables.append("potato")
+vegetables.append("tomato")
+vegetables.append("carrot")
+print(vegetables)
