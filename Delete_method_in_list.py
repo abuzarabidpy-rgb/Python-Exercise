470d1d1c3a3b5b0433e6044dfa863c1130a92ab6
Muhammad_Abuzar <abuzarabid.py@gmail.com>
2026-08-10 22:14:46 +0500

    Delete method
    this functions tells us how we can delete the data from the list

---
+++ Delete_method_in_list.py
@@ -0,0 +1,15 @@
+fruits = ["apple", "mango" ,"orange"]
+# pop method
+fruits.pop()
+fruits.pop(1)
+print(fruits)
+
+fruits = ["mango", "banana", "kiwi"]
+# del method
+del fruits[0]
+print(fruits)
+
+# remove method
+fruits = ["apple", "banana", "orange"]
+fruits.remove("orange")
+print(fruits)
