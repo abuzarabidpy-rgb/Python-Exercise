37c10198921410f37868f83a3d6987c4242da65b
Muhammad_Abuzar <abuzarabid.py@gmail.com>
2026-08-14 09:24:05 +0500

    is and == operator

---
+++ is vs == .py
@@ -0,0 +1,5 @@
+fruits1 = ["orange", "apple", "pear",] 
+fruits3 = ["orange", "apple", "pear",] 
+fruits2 = ["banana", "kiwi", "apple", "banana"]
+# print(fruits1 == fruits3) -------> it tells us equality (values are same)
+# print(fruits1 is fruits3)  -----> it tells us thee value is in the same place
