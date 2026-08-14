9c0392d180f1c839b6af19986b3918b23fef9d69
Muhammad_Abuzar <abuzarabid.py@gmail.com>
2026-08-14 09:19:35 +0500

    split()_method and join()_method

---
+++ split()_and_join()_method.py
@@ -0,0 +1,20 @@
+# split_method:
+user_info = "Abuzar 17".split()
+# print(user_info)  ----> it can add (,)
+
+fruits = "mango orange kiwi".split()
+# print(fruits)
+
+veg = "onion tomato lasin".split()
+# print(veg)
+
+# name, age = input("enter your name and age :").split()
+# print(name)
+# print(age)
+
+# join_method:
+user_info = ['Abuzar', '17']
+# print(','.join(user_info))  ------> it can join the string
+
+user_info2 = ["abuzar", "15"]
+# print(','.join(user_info2))
