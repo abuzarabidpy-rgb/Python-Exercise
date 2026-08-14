62929b67e91c6e444e07783cc499a61bcc40e1be
Muhammad_Abuzar <abuzarabid.py@gmail.com>
2026-08-14 09:20:56 +0500

    for_loop and while_loop

---
+++ for_loop_and_while_loop_in_list.py
@@ -0,0 +1,12 @@
+# for_loop:
+fruits = ['orange', 'mango', 'kiwi', 'banana', 'grapes']
+for i in fruits:
+    print(i)
+
+
+# while_loop:
+fruits = ['orange', 'mango', 'kiwi', 'banana', 'grapes']
+i = 0
+while i < len(fruits):
+    print(fruits[i])
+    i += 1
