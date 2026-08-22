479bfd84071a58f3b63d5864de3fb07919766f43
Muhammad_Abuzar <abuzarabid.py@gmail.com>
2026-08-22 21:54:15 +0500

    tuple_Exercise.py

---
+++ tuple_Exercise.py
@@ -0,0 +1,23 @@
+# tuple:
+
+# tuple with looping:
+mixed = (1, 2, 3, 4, 5)
+for i in mixed:
+    print(i)
+
+# tuple with one element:
+
+nums = (1)
+words = ("world1")
+print(type(nums,))
+print(type(words,))
+
+fruits = "orange", "mango", "apple"
+print(fruits)
+
+# list inside tuple:
+
+favourites = ("abuzar", ["anas", "irfan"])
+favourites[1].pop()
+print(favourites)
+
